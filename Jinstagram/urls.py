3ed41c70193192712016c278f95b869bf63a8e10
byebyemyblue <dlcksgml98@naver.com>
2023-03-24 17:45:00 +0900

    03.24/17:44

--- Jinstagram/urls.py
+++ Jinstagram/urls.py
@@ -19,12 +19,13 @@ from .views import Sub
 from content.views import Main, UploadFeed
 from .settings import MEDIA_URL, MEDIA_ROOT
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
     path('content/', include('content.urls')),
-    path('user/', include('user.urls'))
+    path('user/', include('user.urls')),
 ]
 
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
