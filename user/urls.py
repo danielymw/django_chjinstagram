7536e323282904126c541e3fb208f68e080dfcb4
leesukyung <xlvl369@naver.com>
2023-03-28 14:04:29 +0900

    1차 merge

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import Join, Login, LogOut, UploadProfile, SearchUser
-from .views import Join, Login, LogOut, UploadProfile, Admin, AdminPage
+from .views import Join, Login, LogOut, UploadProfile, Admin, AdminPage, SearchUser
 
 urlpatterns = [
     path('join', Join.as_view()),
     path('login', Login.as_view()),
     path('logout', LogOut.as_view()),
     path('profile/upload', UploadProfile.as_view()),
-    path('search/', SearchUser.as_view(), name='user-search')
+    path('search/', SearchUser.as_view(), name='user-search'),
     # 검색 url 추가
     # WJ user 앱에서 admin 경로를 만들고 view.py의 Admin 클래스 참조
     path('admin', Admin.as_view()),
