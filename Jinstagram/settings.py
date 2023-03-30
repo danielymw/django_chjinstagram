fe9767e4c047a900f88ab2cc9cc9dad0f47c4e70
byebyemyblue <dlcksgml98@naver.com>
2023-03-30 10:07:02 +0900

    CSRF 토큰 v1.1

--- Jinstagram/settings.py
+++ Jinstagram/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware'
+    # CSRF토큰
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
