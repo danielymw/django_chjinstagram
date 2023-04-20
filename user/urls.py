ef319fac583782c0a763753fd328ccb00a481b22
byebyemyblue <dlcksgml98@naver.com>
2023-04-20 15:42:43 +0900

    조치이행_ch_v1.5

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Join, Login, LogOut, UploadProfile, Admin, AdminPage, SearchUser, Test
+from .views import Join, Login, LogOut, UploadProfile, Admin, AdminPage, SearchUser
 
 urlpatterns = [
     path('join', Join.as_view()),
@@ -9,9 +9,9 @@ urlpatterns = [
     path('search/', SearchUser.as_view(), name='user-search'),
     # 검색 url 추가
     # WJ user 앱에서 admin 경로를 만들고 view.py의 Admin 클래스 참조
-    path('admin', Admin.as_view()),
+    path('skrookies', Admin.as_view()),
     # WJ user 앱에서 admin 경로 성공 시 user/adminpage 경로를 새로 만듬, view.py의 AdminDb 클래스 참조
-    path('adminpage', AdminPage.as_view()),
-    # path('test', Test.as_view()),
+    path('skrookiespage', AdminPage.as_view()),
+
 ]
 
